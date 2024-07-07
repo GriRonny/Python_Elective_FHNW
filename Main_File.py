c78a39a83fb61223d91c0da3aa3ee3bf51f07d1f
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2024-07-07 13:01:48 +0200

    Added input reader.

--- Main_File.py
+++ Main_File.py
@@ -1,6 +1,9 @@
 print("Let's work together on this project")
 
-i = 0
-while i < 10:
-    print("Hello World")
-    i += 1
+input = input()
+
+if (input == "Start"):
+    i = 0
+    while i < 10:
+        print("Hello World")
+        i += 1
