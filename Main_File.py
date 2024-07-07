8a782182c2b2f94335c7da6919a4f1e788e02557
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2024-07-07 13:25:22 +0200

    Various shenanigans.

--- Main_File.py
+++ Main_File.py
@@ -1,8 +1,21 @@
 print("Let's work together on this project")
 
-input = input()
+input = input("Please enter a command: \n")
 if (input == "Start"):
     i = 0
     while i < 10:
         print("Hello World")
         i += 1
+else:
+    print("OK")
+
+color_list = ["blue", "grey", "red"]
+dictionary = {
+    "brand": "BMW",
+    "model": "M3",
+    "diesel": False,
+    "year": 2005,
+    "color_options": color_list
+}
+print(len(dictionary))
+
