e6c0b7c814842b64ebf3914714203c91a1d474e1
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2024-07-10 12:48:35 +0200

    Main dashboard structure.

--- main_dashboard.py
+++ main_dashboard.py
@@ -10,15 +10,35 @@ def load_csv(file):
         return None
 
 
-st.header("Test")
-uploaded_csv = st.file_uploader("Choose a CSV file to be processed.", type="csv")
+# Initialize session state
+if 'view' not in st.session_state:
+    st.session_state.view = 'upload'
 
-if uploaded_csv is not None:
-    df = load_csv(uploaded_csv)
-    if df is not None:
-        st.success("CSV file successfully loaded!")
-        st.write(df)
+
+# Function to switch view
+def switch_view(view_name):
+    st.session_state.view = view_name
+    st.rerun()  # Trigger rerun everytime function is called to update view accordingly.
+
+
+if st.session_state.view == 'upload':  # Here we display the "Upload" view if the session state == "upload"
+    st.header("Upload CSV file")
+    uploaded_csv = st.file_uploader("Choose a CSV file to be processed.", type="csv")
+
+    if uploaded_csv is not None:
+        df = load_csv(uploaded_csv)
+        if df is not None:
+            st.success("CSV file successfully loaded!")
+            st.write(df)
+            if st.button("Proceed to Analysis"):
+                switch_view('analysis')
+        else:
+            st.error("Failed to load CSV file.")
     else:
-        st.error("Failed to load CSV file.")
-else:
-    st.info("Please upload a CSV file.")
+        st.info("Please upload a CSV file.")
+elif st.session_state.view == 'analysis':  # Here we display the "Upload" view if the session state == "analysis"
+    st.header("Choose desired analysis scope")
+    st.write("This is the analysis view.")
+
+    if st.button("Go back to Upload"):  # If this button is pressed, the user returns to the defined view.
+        switch_view('upload')  # switch_view function called with parameter "upload"
