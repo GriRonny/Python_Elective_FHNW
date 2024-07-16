55ea1c0048286484739e122f7a1b401035df69e3
RonnyGrieder <ronny.grieder@students.fhnw.ch>
2024-07-16 10:36:49 +0200

    - added new filtering logic to withing the market_scenario.py using the query() function --> highly recommend it! - added average sales per market chart in market_scenario.py

--- market_scenario.py
+++ market_scenario.py
@@ -5,38 +5,53 @@ import altair as alt
 
 
 def market_logic():
-    st.header("Market Section")
+    st.header("Market Analytics")
 
     if st.session_state.df is not None:  # Checking if session state df is not empty
         df = st.session_state.df  # assigning session state df to variable "df"
 
         # Sidebar with checkboxes for markets
-        with st.sidebar:
-            st.header("Filter by Market")
-            markets = df['Market'].unique()
-            selected_markets = [st.checkbox(market, key=market) for market in markets]
-
-        # Create lists of selected markets and segments
-        selected_market_list = [market for market, selected in zip(markets, selected_markets) if selected]
-
-        # Filter the DataFrame based on selected markets and segments
-        if selected_market_list:
-            filtered_df = df[df['Market'].isin(selected_market_list)]
-        else:
-            filtered_df = df
-
-        sales_chart = alt.Chart(filtered_df).mark_bar().encode(
-            x=alt.X('Market:O', sort='-y', title='Market'),
-            y=alt.Y('sum(Sales):Q', title='Total Sales'),
-            color='Market:N',
-            tooltip=['Market', 'sum(Sales)']  # Displayed when hovering over a bar
-        ).properties(
-            width=600,
-            height=400,
-            title='Total Sales by Market'
+        st.sidebar.header("Please Filter Here:")
+        market = st.sidebar.multiselect(
+            "Select Markets:",
+            options=df['Market'].unique(),
+            default=df['Market'].unique()
         )
 
-        st.altair_chart(sales_chart, use_container_width=True)
+        # NEW LOGIC TO FILTER THE DATA USING QUERY() FUNCTION!
+        df_filtered = df.query("Market == @market")
+
+        # --- CODE FOR THE CHARTS ---
+        # Creating two columns for the
+        col1, col2 = st.columns(2)
+        with col1:
+            # --- BARCHART_SALES ---
+            sales_chart = alt.Chart(df_filtered).mark_bar().encode(
+                x=alt.X('Market:O', sort='-y', title='Market'),
+                y=alt.Y('sum(Sales):Q', title='Total Sales'),
+                color='Market:N',
+                tooltip=['Market', 'sum(Sales)']  # Displayed when hovering over a bar
+            ).properties(
+                width=600,
+                height=400,
+                title='Total Sales by Market'
+            )
+
+            st.altair_chart(sales_chart, use_container_width=True)
+
+        with col2:
+            avg_sales_chart = alt.Chart(df_filtered).mark_bar().encode(
+                x=alt.X('Market:O', sort='-y', title='Market'),
+                y=alt.Y('mean(Sales):Q', title='Average Sales'),
+                color='Market:N',
+                tooltip=['Market', 'mean(Sales)']  # Displayed when hovering over a bar
+            ).properties(
+                width=600,
+                height=400,
+                title='Average Total Sales by Market'
+            )
+
+            st.altair_chart(avg_sales_chart, use_container_width=True)
 
     else:
         st.error("No data loaded. Please upload a CSV file.")
