64c54471e4357476f0b14ab384a769b48b7ffe45
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2024-07-28 19:38:02 +0200

    Added further functionalities to sales scenario, including dynamic filters and multi-select widgets.

--- sales_scenario.py
+++ sales_scenario.py
@@ -8,48 +8,82 @@ class SalesScenario:
         pass
 
     def sales_logic(self):
-        st.header("Sales Section")
+        st.header("👷WIP👷: Sales Section")
 
         if st.session_state.df is not None:  # Checking if session state df is not empty
-            df = st.session_state.df  # assigning session state df to variable "df"
+            df = st.session_state.df  # Assigning session state df to variable "df"
 
-            st.bar_chart(data=df["Sales"])
+            # Convert "Order Date" column to datetime
+            df["Order Date"] = pd.to_datetime(df["Order Date"])
 
-            # As long as date is in "Order Date" column, add/append to new list "order_dates" (List Comprehension)
-            order_dates = [date for date in df["Order Date"].unique()]
-
-            # Convert dates using Pandas (.date used to convert to only dates (no time-stamps))
-            order_dates_form = pd.to_datetime(order_dates).date
-
-            # Convert to list
-            order_dates_form_list = list(order_dates_form)
+            # Extract unique years from "Order Date" column and convert to list
+            order_years_list = df["Order Date"].dt.year.unique().tolist()
+            # Extract unique countries from "Countries" column and convert to list
+            country_list = df["Country"].unique().tolist()
 
             # Sidebar for Sales Scenario
             with st.sidebar:
                 st.header("Filter options:")
-                st.slider("Order Date", min_value=min(order_dates_form_list), max_value=max(order_dates_form_list),
-                          value=(min(order_dates_form_list), max(order_dates_form_list)))
 
-                # Returns user to overview view
-                if st.button("Return to overview"):
-                    st.session_state.switch_view('analysis')
+                selected_countries = st.multiselect(
+                    "Select Countries",
+                    options=country_list,
+                    default=country_list[0]
+                )
 
-            # This groups the df by country and appends aggregates of one or more columns
-            sales_summary = df.groupby('Country').agg({'Sales': 'sum', 'Profit': 'mean'}).reset_index()
+                selected_years = st.multiselect(
+                    "Select Years",
+                    options=order_years_list,
+                    default=order_years_list
+                )
 
-            # Rename columns (not working just now)
-            #sales_summary.rename(columns={'Sales': 'Total Sales', 'Profit': 'Mean Profit'}, inplace=True)
+                if selected_years:
+                    df_filtered_by_year = df[df["Order Date"].dt.year.isin(selected_years)]
+                    # Extract unique dates from "Order Date" column and convert to list
+                    order_dates_list = df_filtered_by_year["Order Date"].dt.date.unique().tolist()
 
-            sorted_sales_summary = sales_summary.sort_values(by="Sales", ascending=False)
+                    date_range = st.slider(
+                        "Order Date",
+                        min_value=min(order_dates_list),
+                        max_value=max(order_dates_list),
+                        value=(min(order_dates_list), max(order_dates_list))
+                    )
 
-            top_3_countries = sorted_sales_summary.head(3)
-            flop_3_countries = sorted_sales_summary.tail(3)
+                    # Returns user to overview view
+                    if st.button("Return to overview"):
+                        st.session_state.switch_view('analysis')
 
-            st.write("Countries with the highest Sales:")
-            st.dataframe(top_3_countries)
+            if selected_years:
+                filtered_df = df_filtered_by_year.query(
+                    "`Order Date` >= @pd.Timestamp(@date_range[0]) and `Order Date` <= @pd.Timestamp(@date_range[1]) "
+                    "and `Country` in @selected_countries"
+                )
 
-            st.write("Countries with the lowest Sales:")
-            st.dataframe(flop_3_countries)
+                # Display warning if df empty
+                if filtered_df.empty:
+                    st.warning("No sales data available with current filter applied.", icon='⚠️')
+                else:
+                    # Display the sales bar chart
+                    st.bar_chart(data=filtered_df["Sales"])
+
+                    # This groups the df by country and appends aggregates of one or more columns
+                    sales_summary = df.groupby('Country').agg({'Sales': 'sum', 'Profit': 'mean'}).reset_index()
+
+                    sorted_sales_summary = sales_summary.sort_values(by="Sales", ascending=False)
+
+                    top_3_countries = sorted_sales_summary.head(3)
+                    flop_3_countries = sorted_sales_summary.tail(3)
+
+                    st.write("Countries with the highest Sales:")
+                    st.dataframe(top_3_countries)
+
+                    st.write("Countries with the lowest Sales:")
+                    st.dataframe(flop_3_countries)
+            else:
+                st.warning("No sales data available with current filter applied.", icon="⚠️")
+
+        else:
+            st.warning("No data available. Please upload a dataset.")
 
         if st.button("Go back to Analysis"):
             st.session_state.switch_view('analysis')
