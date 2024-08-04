773d87b8f14bf904e05ed575ea42c0cb87efa93a
Gabriel99S <gabriel.schnelli@students.fhnw.ch>
2024-08-04 17:24:21 +0200

    Gabriel - product_scenario.py

--- main_dashboard.py
+++ main_dashboard.py
@@ -4,6 +4,7 @@ import customer_scenario
 import sales_scenario
 import market_scenario
 import profitability_scenario
+import product_scenario
 
 st.set_page_config(page_title="Business Dashboard", page_icon="📊", layout="centered", initial_sidebar_state="expanded")
 
@@ -22,9 +23,10 @@ st.session_state.switch_view = switch_view  # Make switch_view function accessib
 
 if st.session_state.view == 'upload':  # Display the "Upload" view if the session state == "upload"
 
-    st.header("Welcome to our dashboard!")
+    st.header("Welcome to our dashboard :eggplant:")
 
-    st.write("Please upload the latest **global_superstore.csv** file to start analyzing.")
+    st.write("First, select the latest global_superstore.csv file. \n"
+             "\nNext, enjoy our dashboard that we created!:fire:")
 
     # Define required columns as a constant variable
     REQUIRED_COLUMNS = [
@@ -69,54 +71,63 @@ if st.session_state.view == 'upload':  # Display the "Upload" view if the sessio
         st.info("Please upload a CSV file.")
 
 elif st.session_state.view == 'analysis':  # Here we display the "Upload" view if the session state == "analysis"
-    st.header("Choose your desired analytics:chart_with_upwards_trend:")
-    st.write("Please choose from the following options. After that, click the 'Start Analyzing' button to display "
-             "your selected view and begin the analysis.")
+    st.header("Choose what you want to analyse!:chart_with_upwards_trend:")
+    st.write("Below are the different analyses.")
 
     # Create three parallel sections
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
         st.subheader("Customer Analytics")
-        st.write("- Identify the most and least profitable customers. \n"
-                 "- Gain knowledge about our customer's payment methods. \n"
-                 "- Interactively filter the data.")
+        st.write("- Identify most/least profitable customers \n"
+                 "- Gain insights about payment methods \n"
+                 "- Interactively filter the data")
 
     with col2:
         st.subheader("Market Analytics")
-        st.write("- Compare the total sales by market. \n"
-                 "- Compare the mean sales by market. \n"
-                 "- Interactively filter the data."
+        st.write("- Compare total sales per market \n"
+                 "- Compare mean sales per market \n"
+                 "- Filter the data by desired market"
                  )
 
     with col3:
         st.subheader("Sales Analytics")
-        st.write("- Compare total sales by product categories. \n"
-                 "- Gain insights about sales performance and visualize trends. \n"
-                 "- Interactively filter the data.")
+        st.write("- Identify most/least profitable customers \n"
+                 "- Gain insights about payment methods \n"
+                 "- Interactively filter the data")
 
     with col4:
         st.subheader("Profitability Analytics")
-        st.write("- Gain insights about the company profitability. \n"
-                 "- Visualize profit performance of different segments. \n"
-                 "- Interactively filter the data.")
+        st.write("- Identify most/least profitable customers \n"
+                 "- Gain insights about payment methods \n"
+                 "- Interactively filter the data")
+
+    with col5:
+        st.subheader("Product Analytics")
+        st.write("- Identify top/least selling products \n"
+                 "- Analyse profit by product category \n"
+                 "- Sales trends over time"
+                 )
 
     # Columns to display the buttons that switch to the respective analytics view
-    col5, col6, col7, col8 = st.columns(4)
-    with col5:
-        if st.button("Start analyzing", key="button5"):
-            switch_view('customer')
+    col6, col7, col8, col9, col10 = st.columns(5)
     with col6:
-        if st.button("Start analyzing", key="button6"):
-            switch_view('market')
+        if st.button("Start analysing", key="button5"):
+            switch_view('customer')
     with col7:
-        if st.button("Start analyzing", key="button7"):
-            switch_view('sales')
+        if st.button("Start analysing", key="button6"):
+            switch_view('market')
     with col8:
-        if st.button("Start analyzing", key="button8"):
+        if st.button("Start analysing", key="button7"):
+            switch_view('sales')
+    with col9:
+        if st.button("Start analysing", key="button8"):
             switch_view('profit')
+    with col10:
+        if st.button("Start analysing", key="button9"):
+            switch_view('product')
 
-    if st.button("Go back to Upload", type="secondary"):  # If this button is pressed, the user returns to the defined view.
+    if st.button("Go back to Upload"):  # If this button is pressed, the user returns to the defined view.
         switch_view('upload')  # switch_view function called with parameter "upload"
 
 elif st.session_state.view == 'customer':  # Here we display the "Upload" view if the session state == "customer"
@@ -133,3 +144,6 @@ elif st.session_state.view == 'sales':
 
 elif st.session_state.view == 'profit':
     profitability_scenario.profit_logic()
+
+elif st.session_state.view == 'product':
+    product_scenario.ProductScenario().product_logic()
---
+++ product_scenario.py
@@ -0,0 +1,285 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+class ProductScenario:
+    def __init__(self):  # Empty constructor
+        pass
+
+    def product_logic(self):
+        st.header("Product Analytics")
+
+        # Error Handling here? Because data might be empty?
+        if 'df' in st.session_state and st.session_state.df is not None:  # Checking if session state df is not empty
+            df = st.session_state.df  # Assigning session state df to variable "df"
+
+            # Ensure numeric columns are of correct type
+            df['Sales'] = pd.to_numeric(df['Sales'], errors='coerce')
+            df['Profit'] = pd.to_numeric(df['Profit'], errors='coerce')
+            df['Discount'] = pd.to_numeric(df['Discount'], errors='coerce')
+            df['Shipping Cost'] = pd.to_numeric(df['Shipping Cost'], errors='coerce')
+
+            # Ensure 'Order Date' is datetime
+            df['Order Date'] = pd.to_datetime(df['Order Date'], format='%d %m %Y', errors='coerce')
+
+            # Remove rows with missing Order Dates
+            df = df.dropna(subset=['Order Date'])
+
+            # Sidebar for Product Category Filter
+            with st.sidebar:
+                st.header("Please Filter Here")
+
+                st.subheader("Filter by Product Category")
+                categories = df['Category'].unique()
+                selected_category = st.multiselect("Select Category", categories)
+
+                # Filter DataFrame by selected categories before showing sub-categories
+                if selected_category:
+                    df = df[df['Category'].isin(selected_category)]
+
+                st.subheader("Filter by Sub-Category")
+                sub_categories = df['Sub-Category'].unique()
+                selected_sub_category = st.multiselect("Select Sub-Category", sub_categories)
+
+                # Filter DataFrame by selected sub-categories before showing years
+                if selected_sub_category:
+                    df = df[df['Sub-Category'].isin(selected_sub_category)]
+
+                st.subheader('Select relevant year')
+                df['Order Year'] = df['Order Date'].dt.year
+                unique_years = df['Order Year'].dropna().unique()
+                year_select = st.sidebar.multiselect('Select Year', options=unique_years)
+
+                if year_select:
+                    df = df[df['Order Year'].isin(year_select)]
+
+            # Top-Selling Products & Least-Selling Products
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader("Top-Selling Products")
+                top_selling = df.groupby('Product Name').sum(numeric_only=True)['Sales'].sort_values(
+                    ascending=False).head(10).reset_index()
+                if not top_selling.empty:
+                    top_selling['Rank'] = top_selling['Sales'].rank(method='first', ascending=False)
+                    chart = alt.Chart(top_selling).mark_bar().encode(
+                        x=alt.X('Sales:Q', title='Sales'),
+                        y=alt.Y('Product Name:N', sort='-x', title='Product Name'),
+                        color=alt.Color('Rank:Q', scale=alt.Scale(domain=[1, 10], range=['darkgreen', 'lightgreen']),
+                                        legend=None),
+                        tooltip=['Product Name', 'Sales']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Top-Selling Products.")
+
+            with col2:
+                st.subheader("Least-Selling Products")
+                least_selling = df.groupby('Product Name').sum(numeric_only=True)['Sales'].sort_values().head(
+                    10).reset_index()
+                if not least_selling.empty:
+                    least_selling['Rank'] = least_selling['Sales'].rank(method='first', ascending=True)
+                    chart = alt.Chart(least_selling).mark_bar().encode(
+                        x=alt.X('Sales:Q', title='Sales'),
+                        y=alt.Y('Product Name:N', sort='-x', title='Product Name'),
+                        color=alt.Color('Rank:Q', scale=alt.Scale(domain=[1, 10], range=['darkred', 'lightcoral']),
+                                        legend=None),
+                        tooltip=['Product Name', 'Sales']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Least-Selling Products.")
+
+            # Sales Trends Over Time
+            st.subheader("Sales Trends Over Time")
+            sales_trends = df.resample('M', on='Order Date').sum(numeric_only=True)['Sales'].reset_index()
+            if not sales_trends.empty:
+                sales_trends['Order Date'] = pd.to_datetime(sales_trends['Order Date'])
+                brush = alt.selection_interval(encodings=['x'])
+
+                base = alt.Chart(sales_trends).mark_line(point=True).encode(
+                    x=alt.X('Order Date:T', title='Date'),
+                    y=alt.Y('Sales:Q', title='Sales'),
+                    tooltip=['Order Date:T', 'Sales:Q']
+                ).properties(
+                    width=700,
+                    height=400
+                ).add_selection(
+                    brush
+                )
+
+                trendline = base.transform_regression('Order Date', 'Sales').mark_line(color='red', strokeDash=[5, 5])
+                st.altair_chart(base + trendline)
+            else:
+                st.write("No data available for Sales Trends Over Time.")
+
+            # Sales by Product Category & Sales by Sub-Category
+            col3, col4 = st.columns(2)
+            with col3:
+                st.subheader("Sales by Product Category")
+                sales_by_category = df.groupby('Category').sum(numeric_only=True)['Sales'].sort_values(
+                    ascending=False).reset_index()
+                if not sales_by_category.empty:
+                    sales_by_category['Percentage'] = sales_by_category['Sales'] / sales_by_category[
+                        'Sales'].sum() * 100
+                    sales_by_category['Category'] = sales_by_category['Category'].astype(str)
+
+                    chart = alt.Chart(sales_by_category).mark_arc(innerRadius=50).encode(
+                        theta=alt.Theta(field="Percentage", type="quantitative"),
+                        color=alt.Color(field="Category", type="nominal"),
+                        tooltip=['Category', 'Sales', 'Percentage']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Sales by Product Category.")
+
+            with col4:
+                st.subheader("Sales by Sub-Category")
+                sales_by_sub_category = df.groupby('Sub-Category').sum(numeric_only=True)['Sales'].sort_values(
+                    ascending=False).reset_index()
+                if not sales_by_sub_category.empty:
+                    chart = alt.Chart(sales_by_sub_category).mark_bar().encode(
+                        x=alt.X('Sales:Q', title='Sales'),
+                        y=alt.Y('Sub-Category:N', sort='-x', title='Sub-Category'),
+                        tooltip=['Sub-Category', 'Sales']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Sales by Sub-Category.")
+
+            # Profit by Product Category & Profit by Sub-Category
+            col5, col6 = st.columns(2)
+            with col5:
+                st.subheader("Profit by Product Category")
+                profit_by_category = df.groupby('Category').sum(numeric_only=True)['Profit'].sort_values(
+                    ascending=False).reset_index()
+                if not profit_by_category.empty:
+                    profit_by_category['Percentage'] = profit_by_category['Profit'] / profit_by_category[
+                        'Profit'].sum() * 100
+                    profit_by_category['Category'] = profit_by_category['Category'].astype(str)
+
+                    chart = alt.Chart(profit_by_category).mark_arc(innerRadius=50).encode(
+                        theta=alt.Theta(field="Percentage", type="quantitative"),
+                        color=alt.Color(field="Category", type="nominal"),
+                        tooltip=['Category', 'Profit', 'Percentage']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Profit by Product Category.")
+
+            with col6:
+                st.subheader("Profit by Sub-Category")
+                profit_by_sub_category = df.groupby('Sub-Category').sum(numeric_only=True)['Profit'].sort_values(
+                    ascending=False).reset_index()
+                if not profit_by_sub_category.empty:
+                    chart = alt.Chart(profit_by_sub_category).mark_bar().encode(
+                        x=alt.X('Profit:Q', title='Profit'),
+                        y=alt.Y('Sub-Category:N', sort='-x', title='Sub-Category'),
+                        tooltip=['Sub-Category', 'Profit']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Profit by Sub-Category.")
+
+            # Average Discount by Category & Average Discount by Sub-Category
+            col7, col8 = st.columns(2)
+            with col7:
+                st.subheader("Average Discount by Category")
+                avg_discount_by_category = df.groupby('Category').mean(numeric_only=True)['Discount'].sort_values(
+                    ascending=False).reset_index()
+                if not avg_discount_by_category.empty:
+                    avg_discount_by_category['Percentage'] = avg_discount_by_category['Discount'] / \
+                                                             avg_discount_by_category['Discount'].sum() * 100
+
+                    avg_discount_by_category['Category'] = avg_discount_by_category['Category'].astype(str)
+
+                    chart = alt.Chart(avg_discount_by_category).mark_arc(innerRadius=50).encode(
+                        theta=alt.Theta(field="Percentage", type="quantitative"),
+                        color=alt.Color(field="Category", type="nominal"),
+                        tooltip=['Category', 'Discount', 'Percentage']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Average Discount by Category.")
+
+            with col8:
+                st.subheader("Average Discount by Sub-Category")
+                avg_discount_by_sub_category = df.groupby('Sub-Category').mean(numeric_only=True)['Discount'].sort_values(ascending=False).reset_index()
+                if not avg_discount_by_sub_category.empty:
+                    chart = alt.Chart(avg_discount_by_sub_category).mark_bar().encode(
+                        x=alt.X('Discount:Q', title='Average Discount'),
+                        y=alt.Y('Sub-Category:N', sort='-x', title='Sub-Category'),
+                        tooltip=['Sub-Category', 'Discount']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Average Discount by Sub-Category.")
+
+            # Shipping Cost Analysis by Category & Shipping Cost Analysis by Sub-Category
+            col9, col10 = st.columns(2)
+            with col9:
+                st.subheader("Shipping Cost Analysis by Category")
+                shipping_cost_by_category = df.groupby('Category').mean(numeric_only=True)['Shipping Cost'].sort_values(ascending=False).reset_index()
+                if not shipping_cost_by_category.empty:
+                    shipping_cost_by_category['Percentage'] = shipping_cost_by_category['Shipping Cost'] / shipping_cost_by_category['Shipping Cost'].sum() * 100
+                    shipping_cost_by_category['Category'] = shipping_cost_by_category['Category'].astype(str)
+
+                    chart = alt.Chart(shipping_cost_by_category).mark_arc(innerRadius=50).encode(
+                        theta=alt.Theta(field="Percentage", type="quantitative"),
+                        color=alt.Color(field="Category", type="nominal"),
+                        tooltip=['Category', 'Shipping Cost', 'Percentage']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Shipping Cost by Category.")
+
+            with col10:
+                st.subheader("Shipping Cost Analysis by Sub-Category")
+                shipping_cost_by_sub_category = df.groupby('Sub-Category').mean(numeric_only=True)['Shipping Cost'].sort_values(ascending=False).reset_index()
+                if not shipping_cost_by_sub_category.empty:
+                    chart = alt.Chart(shipping_cost_by_sub_category).mark_bar().encode(
+                        x=alt.X('Shipping Cost:Q', title='Shipping Cost'),
+                        y=alt.Y('Sub-Category:N', sort='-x', title='Sub-Category'),
+                        tooltip=['Sub-Category', 'Shipping Cost']
+                    ).properties(
+                        width=350,
+                        height=400
+                    )
+                    st.altair_chart(chart)
+                else:
+                    st.write("No data available for Shipping Cost by Sub-Category.")
+
+        else:
+            st.error("No data loaded. Please upload a CSV file.")
+
+        with st.sidebar:
+            if st.button("Return to overview"):
+                st.session_state.switch_view('analysis')
